6893989c3a7e5804b84b64ac0a09daa72d87d5d8
Nicholas Morrissey <nicksjm@amazon.com>
2022-09-14 20:41:03 +0100

    Add eslint and fix linting

--- lib/infrastructure-stack.ts
+++ lib/infrastructure-stack.ts
@@ -3,14 +3,14 @@ import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class InfrastructureStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
-    super(scope, id, props);
+    constructor(scope: Construct, id: string, props?: StackProps) {
+        super(scope, id, props);
 
-    // The code that defines your stack goes here
+        // The code that defines your stack goes here
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'InfrastructureQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+        // example resource
+        // const queue = new sqs.Queue(this, 'InfrastructureQueue', {
+        //   visibilityTimeout: cdk.Duration.seconds(300)
+        // });
+    }
 }
