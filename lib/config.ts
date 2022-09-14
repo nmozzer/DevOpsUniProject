5e780e145e18f41454ee645752ad690b89c2f96b
Nicholas Morrissey <nicksjm@amazon.com>
2022-09-14 21:00:03 +0100

    Add pipeline stack and update bin file

---
+++ lib/config.ts
@@ -0,0 +1,9 @@
+interface Config {
+    account: string;
+    region: string;
+}
+
+const CONFIG = {
+    account: 431367909633,
+    region: 'eu-west-1',
+};
---
+++ lib/pipeline.ts
@@ -0,0 +1,19 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+
+const GITHUB_SOURCE_REPO = 'nmozzer/DevOpsUniProject';
+
+export class PipelineStack extends cdk.Stack {
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
+
+        const pipeline = new CodePipeline(this, 'Pipeline', {
+            pipelineName: 'DevOpsAssignmentPipeline',
+            synth: new ShellStep('Synth', {
+                input: CodePipelineSource.gitHub(GITHUB_SOURCE_REPO, 'main'),
+                commands: ['npm ci', 'npm run build', 'npx cdk synth'],
+            }),
+        });
+    }
+}
