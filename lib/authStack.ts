95e12a825511808758650e1d073604374d68c7ec
Nicholas Morrissey <nicksjm@amazon.com>
2022-09-17 20:51:49 +0100

    Add AuthStack with cognito UserPool

---
+++ lib/authStack.ts
@@ -0,0 +1,35 @@
+import { Construct } from 'constructs';
+import * as cdk from 'aws-cdk-lib';
+import * as cognito from 'aws-cdk-lib/aws-cognito';
+
+export class AuthStack extends cdk.Stack {
+    readonly userPool: cognito.IUserPool;
+    readonly userPoolWebClient: cognito.IUserPoolClient;
+
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
+
+        this.userPool = new cognito.UserPool(this, 'FFUserPool', {
+            userPoolName: 'FFUserPool',
+            selfSignUpEnabled: true,
+        });
+
+        this.userPoolWebClient = this.userPool.addClient('UserPoolWebClient', {
+            authFlows: {
+                userPassword: true,
+                userSrp: true,
+            },
+            generateSecret: false,
+        });
+
+        this.createUserPoolGroup('Admin');
+        this.createUserPoolGroup('User');
+    }
+
+    private createUserPoolGroup(groupName: string) {
+        new cognito.CfnUserPoolGroup(this, groupName, {
+            userPoolId: this.userPool.userPoolId,
+            groupName,
+        });
+    }
+}
---
+++ test/authStack.test.ts
@@ -0,0 +1,14 @@
+import { AuthStack } from './../lib/authStack';
+import { App } from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+
+describe('AuthStack', () => {
+    it('Has expected stack resources', () => {
+        const app = new App();
+        const stack = new AuthStack(app, 'Test');
+
+        Template.fromStack(stack).hasResourceProperties('AWS::Cognito::UserPool', {
+            UserPoolName: 'FFUserPool',
+        });
+    });
+});
