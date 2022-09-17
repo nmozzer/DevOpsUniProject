3130e5f2baf066b27be9eda5af5663a9aceb5ac2
Nicholas Morrissey <nicksjm@amazon.com>
2022-09-17 20:53:03 +0100

    Add AuthStack to stage.ts

--- lib/stage.ts
+++ lib/stage.ts
@@ -1,11 +1,13 @@
 import { InfrastructureStack } from './infrastructure-stack';
 import { Stage, StageProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { AuthStack } from './authStack';
 
 export class PipelineStage extends Stage {
     constructor(scope: Construct, id: string, props?: StageProps) {
         super(scope, id, props);
 
         new InfrastructureStack(this, 'DevOpsInfraStack');
+        new AuthStack(this, 'DevOpsAuthStack');
     }
 }
