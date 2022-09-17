2cdd3266998bce2279bb3ff254465f7fa0beee8e
Nicholas Morrissey <nicksjm@amazon.com>
2022-09-18 00:25:01 +0100

    Fix cdk.out path for pipeline

--- packages/cdk/lib/pipeline.ts
+++ packages/cdk/lib/pipeline.ts
@@ -24,6 +24,7 @@ export class PipelineStack extends cdk.Stack {
                     'npm run test',
                     'npm run cdk-synth',
                 ],
+                primaryOutputDirectory: 'packages/cdk/cdk.out',
             }),
         });
 
