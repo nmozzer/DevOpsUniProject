5fde52b1882ce2c4da80c49beba8c45c09386699
Nicholas Morrissey <nicksjm@amazon.com>
2022-09-18 20:30:44 +0100

    Add tailwindCSS to frontend

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './css/App.css';
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
 
 export default App;
