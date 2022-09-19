b5405b4aa4426f18a0cad3b059b26b9d2c060e33
Nicholas Morrissey <nicksjm@amazon.com>
2022-09-19 13:55:06 +0100

    Add auth hooks for validating sign up forms

---
+++ packages/frontend/src/cognito/validationHooks.tsx
@@ -0,0 +1,73 @@
+import { useState, useEffect } from 'react';
+
+export const validateEmail = (initialValue: string) => {
+    const [email, setEmail] = useState(initialValue);
+    const [emailIsValid, setEmailIsValid] = useState(true);
+
+    useEffect(() => {
+        if (email.length === 0) {
+            setEmailIsValid(true);
+            return;
+        }
+
+        const isValid = email ? true : false;
+
+        setEmailIsValid(isValid);
+    }, [email]);
+
+    return { email, setEmail, emailIsValid };
+};
+
+export const validatePassword = (initialValue: string) => {
+    const [password, setPassword] = useState(initialValue);
+    const [passwordIsValid, setPasswordIsValid] = useState(true);
+
+    useEffect(() => {
+        if (password.length === 0) {
+            setPasswordIsValid(true);
+            return;
+        }
+
+        const isValid = password.length <= 8 ? true : false;
+
+        setPasswordIsValid(isValid);
+    }, [password]);
+
+    return { password, setPassword, passwordIsValid };
+};
+
+export const validateUsername = (initialValue: string) => {
+    const [username, setUsername] = useState(initialValue);
+    const [usernameIsValid, setUsernameIsValid] = useState(true);
+
+    useEffect(() => {
+        if (username.length === 0) {
+            setUsernameIsValid(true);
+            return;
+        }
+
+        const isValid = username.length <= 8 ? true : false;
+
+        setUsernameIsValid(isValid);
+    }, [username]);
+
+    return { username, setUsername, usernameIsValid };
+};
+
+export const validateVerificationCode = (initialValue: string) => {
+    const [code, setCode] = useState(initialValue);
+    const [codeIsValid, setCodeIsValid] = useState(true);
+
+    useEffect(() => {
+        if (code.length === 0) {
+            setCodeIsValid(true);
+            return;
+        }
+
+        const isValid = code.length <= 6 ? true : false;
+
+        setCodeIsValid(isValid);
+    }, [code]);
+
+    return { code, setCode, codeIsValid };
+};
