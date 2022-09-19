cec6da55738df3cd1aba7e92e5dc6c7f550f42fc
Nicholas Morrissey <nicksjm@amazon.com>
2022-09-19 19:45:34 +0100

    Protect MainPage behind sign-up

--- packages/frontend/src/components/MainPage.tsx
+++ packages/frontend/src/components/MainPage.tsx
@@ -1,11 +1,32 @@
+import { useContext, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { AuthContext, AuthState } from '../cognito/context';
 import IdeaTable from './IdeaTable';
 
 export const MainPage = () => {
-    return (
-        <div className="p-5">
-            <IdeaTable />
-        </div>
-    );
+    const authContext = useContext(AuthContext);
+    const navigate = useNavigate();
+    console.log(authContext);
+
+    console.log(authContext.authState);
+
+    const signedIn = authContext.authState === AuthState.SignedIn ? true : false;
+    useEffect(() => {
+        if (!signedIn) {
+            console.log(authContext.authState);
+            navigate('/signIn');
+        }
+    });
+
+    if (signedIn) {
+        return (
+            <div className="p-5">
+                <IdeaTable />
+            </div>
+        );
+    }
+
+    return <></>;
 };
 
 export default MainPage;
