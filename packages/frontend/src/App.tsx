5e01cce09551e45f3903150558a8cbd4fb7d348e
Nicholas Morrissey <nicksjm@amazon.com>
2022-09-19 21:24:46 +0100

    Fix Auth Routes

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -5,12 +5,12 @@ import { AppBar, IconButton, Toolbar, Typography } from '@mui/material';
 import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 import MenuIcon from '@mui/icons-material/Menu';
 import MainPage from './components/MainPage';
-import SignIn from './components/auth/routes/SignIn';
 import AuthProvider from './cognito/context';
-import Verify from './components/auth/routes/Verify';
-import Resend from './components/auth/routes/Resend';
+import ChangePassword from './components/auth/routes/ChangePassword';
 import ForgotPassword from './components/auth/routes/ForgotPassword';
-import AuthRoutes from './components/auth/routes/AuthRoutes';
+import Resend from './components/auth/routes/Resend';
+import SignIn from './components/auth/routes/SignIn';
+import Verify from './components/auth/routes/Verify';
 
 function App() {
     return (
@@ -42,7 +42,11 @@ function App() {
                     <Routes>
                         <Route path="/" element={<LandingPage />} />
                         <Route path="/ideas" element={<MainPage />} />
-                        <AuthRoutes />
+                        <Route path="/signIn" element={<SignIn />} />
+                        <Route path="/verify" element={<Verify />} />
+                        <Route path="/resendCode" element={<Resend />} />
+                        <Route path="/forgotPassword" element={<ForgotPassword />} />
+                        <Route path="/changePassword" element={<ChangePassword />} />
                     </Routes>
                 </Router>
             </AuthProvider>
