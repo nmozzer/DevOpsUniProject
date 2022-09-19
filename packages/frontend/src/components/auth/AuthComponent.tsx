d57e71392fbe5a062203705e98bd16ad51835ed1
Nicholas Morrissey <nicksjm@amazon.com>
2022-09-19 14:10:09 +0100

    Add generic AuthComponent

---
+++ packages/frontend/src/components/auth/AuthComponent.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import TextField from '@mui/material/TextField';
+
+interface AuthComponentProps {
+    validLabel: string;
+    invalidLabel: string;
+    isValid: boolean;
+    setProp: (_: string) => void;
+    passwordType?: string;
+}
+
+export const AuthComponent = ({ validLabel, invalidLabel, isValid, setProp, passwordType }: AuthComponentProps) => {
+    return (
+        <TextField
+            fullWidth
+            type={passwordType}
+            variant="outlined"
+            label={isValid ? validLabel : invalidLabel}
+            error={!isValid}
+            onChange={(evt: React.ChangeEvent<HTMLTextAreaElement>) => {
+                setProp(evt.target.value);
+            }}
+        />
+    );
+};
