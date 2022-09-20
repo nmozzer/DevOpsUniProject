21112d1531271bfe13a0288a3100919fb8c34c63
Nicholas Morrissey <nicksjm@amazon.com>
2022-09-20 12:37:10 +0100

    Change cognitoConfig to reflect new values

--- packages/frontend/src/cognito/cognitoConfig.ts
+++ packages/frontend/src/cognito/cognitoConfig.ts
@@ -1,6 +1,6 @@
 import { ICognitoUserPoolData } from 'amazon-cognito-identity-js';
 
 export const COGNITO_CONFIG: ICognitoUserPoolData = {
-    UserPoolId: 'eu-west-1_65GwMLsQR',
-    ClientId: '1m0nraabmf3r6l1lb8k8f2ok83',
+    UserPoolId: 'us-east-1_2wMtC9hHd',
+    ClientId: '5go2kv48ft65dqinuj5gu72drq',
 };
