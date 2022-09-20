aa22e4f5a40c0dac4ab856abc278da2042d59ae5
Nicholas Morrissey <nicksjm@amazon.com>
2022-09-20 13:14:34 +0100

    Fix unWrappedHandler unused linting error

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,13 +1,15 @@
 import { APIGatewayProxyResultV2 } from 'aws-lambda';
 import { RawRequest } from './types';
 
-const unwrappedHandler = async () => {
+const unWrappedHandler = async () => {
     return {
         statusCode: 200,
         body: 'Hello World',
     };
 };
 
-export const handler = async (rawRequest: RawRequest): Promise<APIGatewayProxyResultV2> => {};
+export const handler = async (rawRequest: RawRequest): Promise<APIGatewayProxyResultV2> => {
+    return await unWrappedHandler();
+};
 
 export default handler;
