faac2fe64cc68d933139db00564fe5503536c807
Nicholas Morrissey <nicksjm@amazon.com>
2022-09-23 11:38:59 +0100

    Unmarshall dynamoDb response

--- packages/backend/src/handlers/getAllIdeas.ts
+++ packages/backend/src/handlers/getAllIdeas.ts
@@ -1,6 +1,7 @@
-import { ExecuteStatementCommand, ExecuteStatementCommandInput } from '@aws-sdk/client-dynamodb';
+import { AttributeValue, ExecuteStatementCommand, ExecuteStatementCommandInput } from '@aws-sdk/client-dynamodb';
 import { DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb';
 import { successResponse } from '../util/apiResponses';
+import { unmarshall } from '@aws-sdk/util-dynamodb';
 
 export const getAllIdeas = async (dbClient: DynamoDBDocumentClient, tableName: string) => {
     const query: ExecuteStatementCommandInput = {
@@ -10,5 +11,11 @@ export const getAllIdeas = async (dbClient: DynamoDBDocumentClient, tableName: s
     const response = await dbClient.send(new ExecuteStatementCommand(query));
     console.log('Retrieved Items Successfully');
 
-    return successResponse(response?.Items);
+    const unMarshalledResponse = unMarshallResponse(response?.Items);
+
+    return successResponse(unMarshalledResponse);
+};
+
+const unMarshallResponse = (items?: Record<string, AttributeValue>[]) => {
+    return items?.map((item) => unmarshall(item));
 };
