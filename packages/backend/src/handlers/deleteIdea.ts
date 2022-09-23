1b74072135efd00d5bb21960f84ba0b32255afb1
Nicholas Morrissey <nicksjm@amazon.com>
2022-09-23 21:08:32 +0100

    Change update handler to delete and add

--- packages/backend/src/handlers/deleteIdea.ts
+++ packages/backend/src/handlers/deleteIdea.ts
@@ -1,7 +1,6 @@
 import { successResponse } from './../util/apiResponses';
 import { DeleteRequest } from './../types';
-import { DynamoDBDocumentClient, ExecuteStatementCommandInput } from '@aws-sdk/lib-dynamodb';
-import { ExecuteStatementCommand } from '@aws-sdk/client-dynamodb';
+import { DynamoDBDocumentClient, ExecuteStatementCommand, ExecuteStatementCommandInput } from '@aws-sdk/lib-dynamodb';
 
 export const deleteIdea = async (request: DeleteRequest, dbClient: DynamoDBDocumentClient, tableName: string) => {
     const { nameDeletion } = request;
@@ -11,8 +10,8 @@ export const deleteIdea = async (request: DeleteRequest, dbClient: DynamoDBDocum
     }
 
     const query: ExecuteStatementCommandInput = {
-        Statement: `DELETE FROM "${tableName}" where PK=?`,
-        Parameters: [{ S: nameDeletion }],
+        Statement: `DELETE * FROM "${tableName}" where PK=?`,
+        Parameters: [{ PK: nameDeletion }],
     };
 
     const response = await dbClient.send(new ExecuteStatementCommand(query));
--- packages/backend/src/handlers/updateIdea.ts
+++ packages/backend/src/handlers/updateIdea.ts
@@ -1,34 +1,38 @@
-import { DynamoDBDocumentClient, ExecuteStatementCommand, ExecuteStatementCommandInput } from '@aws-sdk/lib-dynamodb';
+import {
+    DynamoDBDocumentClient,
+    BatchExecuteStatementCommandInput,
+    BatchExecuteStatementCommand,
+} from '@aws-sdk/lib-dynamodb';
 import { AddOrUpdateRequest } from '../types';
 import { successResponse } from '../util/apiResponses';
 
 export const updateIdea = async (request: AddOrUpdateRequest, dbClient: DynamoDBDocumentClient, tableName: string) => {
     const { oldName, name, system, beans, difficulty, creator, assigned } = request;
 
-    const query: ExecuteStatementCommandInput = {
-        Statement: `UPDATE "${tableName}" 
-            SET PK='?' 
-            SET system='?' 
-            SET beans='?' 
-            SET difficulty='?' 
-            SET creator='?' 
-            SET assigned='?' 
-            where PK='?'`,
-        Parameters: [
-            { S: name },
-            { S: system },
-            { S: beans.toString() },
-            { S: difficulty },
-            { S: creator },
-            { S: assigned.toString() },
-            { S: oldName },
+    const query: BatchExecuteStatementCommandInput = {
+        Statements: [
+            {
+                Statement: `DELETE * FROM "${tableName}" where PK=?`,
+                Parameters: [{ PK: oldName }],
+            },
+            {
+                Statement: `INSERT INTO "${tableName}" value {'PK':?, 'system':?, 'beans':?, 'difficulty':?, 'creator':?, 'assigned':?}`,
+                Parameters: [
+                    { S: name },
+                    { S: system },
+                    { S: beans.toString() },
+                    { S: difficulty },
+                    { S: creator },
+                    { S: assigned.toString() },
+                ],
+            },
         ],
     };
 
     console.log(query);
 
-    const response = await dbClient.send(new ExecuteStatementCommand(query));
+    const response = await dbClient.send(new BatchExecuteStatementCommand(query));
     console.log('Updated Item Successfully');
 
-    return successResponse(response?.Items);
+    return successResponse(response?.Responses);
 };
